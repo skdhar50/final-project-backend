2f54746f426555e70e83b1551d6d2252f962e06e
skdhar50 <skdhar50@gmail.com>
2022-03-27 01:36:31 +0600

    user model updated

--- models/user.js
+++ models/user.js
@@ -1,27 +1,40 @@
 const { Schema, model } = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-module.exports.User = model(
-	"User",
-	Schema(
-		{
-			name: {
-				type: String,
-				required: true,
-			},
-			email: {
-				type: String,
-				required: true,
-				unique: true,
-			},
-			password: {
-				type: String,
-			},
-			role: {
-				type: String,
-				enum: ["admin", "user"],
-				default: "user",
-			},
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
 		},
-		{ timestamps: true }
-	)
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			maxlength: 1024,
+			required: true,
+		},
+		role: {
+			type: String,
+			enum: ["admin", "user"],
+			default: "user",
+		},
+	},
+	{ timestamps: true }
 );
+
+// Generate the json web token for auth user
+userSchema.methods.generateToken = function () {
+	const token = jwt.sign(
+		{ _id: this._id, email: this.email, name: this.name, role: this.role },
+		process.env.TOKEN_SECRET,
+		{ expiresIn: "7d" }
+	);
+
+	return token;
+};
+
+module.exports.User = model("User", userSchema);
