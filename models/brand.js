bc24d010ec6e8cce71c78e5664e55d2acdcbf140
skdhar50 <skdhar50@gmail.com>
2022-03-29 00:15:02 +0600

    brand model added

--- models/brand.js
+++ models/brand.js
@@ -1,3 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-module.exports.Brand = model("Brand", Schema({}, { timestamps: true }));
+module.exports.Brand = model(
+	"Brand",
+	Schema(
+		{
+			name: {
+				type: String,
+				unique: true,
+			},
+			status: {
+				type: String,
+				enum: ["active", "inactive"],
+				default: "active",
+			},
+			icon: String,
+		},
+		{ timestamps: true }
+	)
+);
