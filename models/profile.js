f799067c7772d99a48a2d84ee4a1fbe8c3773e9e
skdhar50 <skdhar50@gmail.com>
2022-03-28 23:49:57 +0600

    profile model added

--- models/profile.js
+++ models/profile.js
@@ -1,3 +1,41 @@
 const { Schema, model } = require("mongoose");
 
-module.exports.Profile = model("Profile", Schema({}, { timestamps: true }));
+module.exports.Profile = model(
+	"Profile",
+	Schema(
+		{
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+			phone: {
+				type: String,
+				required: true,
+			},
+			address1: {
+				type: String,
+				required: true,
+			},
+			address2: {
+				type: String,
+				required: true,
+			},
+			city: {
+				type: String,
+				required: true,
+			},
+			state: {
+				type: String,
+			},
+			postalCode: {
+				type: String,
+				required: true,
+			},
+			country: {
+				type: String,
+				required: true,
+			},
+		},
+		{ timestamps: true }
+	)
+);
