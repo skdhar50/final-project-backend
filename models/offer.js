d2a217334422f8c0fca5167b34b5e632e8650514
skdhar50 <skdhar50@gmail.com>
2022-03-29 00:44:46 +0600

    offer model added

--- models/offer.js
+++ models/offer.js
@@ -1,4 +1,39 @@
 const { Schema, model } = require("mongoose");
 
-module.exports.Offer = model("Offer", Schema({}, { timestamps: true }));
-
+module.exports.Offer = model(
+	"Offer",
+	Schema(
+		{
+			name: String,
+			startDate: Date,
+			endDate: Date,
+			description: String,
+			discountAmount: Number,
+			limit: Number,
+			status: {
+				type: String,
+				enum: ["active", "inactive"],
+				default: "active",
+			},
+			category: [
+				{
+					type: Schema.Types.ObjectId,
+					ref: "Category",
+				},
+			],
+			brand: [
+				{
+					type: Schema.Types.String,
+					ref: "Brand",
+				},
+			],
+			products: [
+				{
+					type: Schema.Types.ObjectId,
+					ref: "Products",
+				},
+			],
+		},
+		{ timestamps: true }
+	)
+);
