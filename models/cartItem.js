64a93bf19f167b80983e364e8b4e2afe732927bf
skdhar50 <skdhar50@gmail.com>
2022-03-29 00:39:44 +0600

    order model added

--- models/cartItem.js
+++ models/cartItem.js
@@ -1,26 +1,26 @@
 const { Schema, model } = require("mongoose");
 
-module.exports.CartItem = model(
-	"CartItem",
-	Schema(
-		{
-			product: {
-				type: Schema.Types.ObjectId,
-				ref: "Product",
-				required: true,
-			},
-			count: {
-				type: Number,
-				default: 1,
-				min: 1,
-				max: 5,
-			},
-			user: {
-				type: Schema.Types.ObjectId,
-				ref: "User",
-				required: true,
-			},
+const CartItemSchema = Schema(
+	{
+		product: {
+			type: Schema.Types.ObjectId,
+			ref: "Product",
+			required: true,
 		},
-		{ timestamps: true }
-	)
+		count: {
+			type: Number,
+			default: 1,
+			min: 1,
+			max: 5,
+		},
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+	},
+	{ timestamps: true }
 );
+
+model.exports.CartItemSchema = CartItemSchema;
+module.exports.CartItem = model("CartItem", CartItemSchema);
--- models/order.js
+++ models/order.js
@@ -1,4 +1,66 @@
 const { Schema, model } = require("mongoose");
+const { CartItemSchema } = require("./cartItem");
 
-module.exports.Order = model("Order", Schema({}, { timestamps: true }));
-
+module.exports.Order = model(
+	"Order",
+	Schema(
+		{
+			cartItem: [CartItemSchema],
+			transaction_id: {
+				type: String,
+				unique: true,
+			},
+			address: {
+				phone: {
+					type: String,
+					required: true,
+				},
+				address1: {
+					type: String,
+					required: true,
+				},
+				address2: {
+					type: String,
+					required: true,
+				},
+				city: {
+					type: String,
+					required: true,
+				},
+				state: {
+					type: String,
+				},
+				postalCode: {
+					type: String,
+					required: true,
+				},
+				country: {
+					type: String,
+					required: true,
+				},
+			},
+			paymentStatus: {
+				type: String,
+				enum: ["pending", "complete"],
+				default: "pending",
+			},
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: "User",
+			},
+			status: {
+				type: String,
+				enum: [
+					"pending",
+					"processing",
+					"shipped",
+					"delivered",
+					"returned",
+					"cancelled",
+				],
+				default: "pending",
+			},
+		},
+		{ timestamps: true }
+	)
+);
