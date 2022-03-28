f4b1b4990eb74715096f09d8f991106923e43190
skdhar50 <skdhar50@gmail.com>
2022-03-29 00:10:40 +0600

    review model added

--- models/review.js
+++ models/review.js
@@ -1,3 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-module.exports.Review = model("Review", Schema({}, { timestamps: true }));
+module.exports.Review = model(
+	"Review",
+	Schema(
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: "User",
+			},
+			product: {
+				type: Schema.Types.ObjectId,
+				ref: "Product",
+			},
+			review: String,
+			rating: String,
+		},
+		{ timestamps: true }
+	)
+);
