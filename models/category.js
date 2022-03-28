e626e1f10320e4f63cb2a7cd032bed6a9b571b3f
skdhar50 <skdhar50@gmail.com>
2022-03-29 00:13:18 +0600

    category model added

--- models/category.js
+++ models/category.js
@@ -1,3 +1,19 @@
 const { Schema, model } = require("mongoose");
 
-module.exports.Category = model("Category", Schema({}, { timestamps: true }));
+module.exports.Category = model(
+	"Category",
+	Schema(
+		{
+			name: {
+				type: String,
+				unique: true,
+			},
+			status: {
+				type: String,
+				enum: ["active", "inactive"],
+				default: "active",
+			},
+		},
+		{ timestamps: true }
+	)
+);
