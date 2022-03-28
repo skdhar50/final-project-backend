2b73aecdb016ef759cabdc7f6c7db003b598a655
skdhar50 <skdhar50@gmail.com>
2022-03-29 00:20:39 +0600

    cartitem model added

--- models/cartItem.js
+++ models/cartItem.js
@@ -1,4 +1,26 @@
 const { Schema, model } = require("mongoose");
 
-module.exports.CartItem = model("CartItem", Schema({}, { timestamps: true }));
-
+module.exports.CartItem = model(
+	"CartItem",
+	Schema(
+		{
+			product: {
+				type: Schema.Types.ObjectId,
+				ref: "Product",
+				required: true,
+			},
+			count: {
+				type: Number,
+				default: 1,
+				min: 1,
+				max: 5,
+			},
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: "User",
+				required: true,
+			},
+		},
+		{ timestamps: true }
+	)
+);
