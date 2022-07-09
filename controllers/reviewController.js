667f8990a141e15f9040b7f437fdb83ce2c4cf4a
skdhar50 <skdhar50@gmail.com>
2022-07-09 21:00:58 +0600

    review image upload added

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,9 +3,11 @@ const formidable = require("formidable");
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
+const upload = require("../middlewares/multer");
+const multer = require("multer");
 
 module.exports.getReviews = async (req, res) => {
-	const reviews = await Review.find({ product: req.params.id }).populate(
+	const reviews = await Review.find({ product: req.params.id }).sort({createdAt : -1}).populate(
 		"user"
 	);
 	let tempReviews = reviews.map((review) =>
@@ -26,84 +28,31 @@ module.exports.getReviews = async (req, res) => {
 		)
 	);
 
-	if (tempReviews.length > 0) {
-		console.log(tempReviews);
-	}
-
-	return res.status(200).send(reviews);
+	return res.status(200).send(tempReviews);
 };
 
 module.exports.postReview = async (req, res) => {
-	const form = new formidable.IncomingForm();
+	upload(req, res, async function (err) {
+		if (err instanceof multer.MulterError) {
+			return res.status(404).send("Invalid files");
+		} else if (err) {
+			return res.status(500).send("Something went wrong");
+		}
 
-	form.keepExtensions = true;
-	form.multiples = true;
-	form.maxFieldsSize = 10 * 1024 * 1024;
-	form.uploadDir = __dirname + "/../storages/reviews";
+		let photos = [];
 
-	let photos = [];
-	let inputData = {};
-
-	form
-		.on("error", function (err) {
-			throw err;
-		})
-		.on("field", function (field, value) {
-			// console.log(field, value);
-			inputData = {
-				...inputData,
-				[field]: value,
-			};
-		})
-		/* this is where the renaming happens */
-		.on("fileBegin", function (name, file) {
-			//rename the incoming file to the file's name
-			file.filepath =
-				form.uploadDir +
-				"/" +
-				file.newFilename +
-				"." +
-				file.mimetype.split("/")[1];
-
-			photos.push(file.newFilename + "." + file.mimetype.split("/")[1]);
-		})
-		.on("file", function (field, file) {
-			//On file received
-		})
-		.on("progress", function (bytesReceived, bytesExpected) {
-			//self.emit('progess', bytesReceived, bytesExpected)
-			// var percent = ((bytesReceived / bytesExpected) * 100) | 0;
-			// process.stdout.write("Uploading: %" + percent + "\r");
-		})
-		.on("end", async function () {
-			inputData = {
-				...inputData,
-				photos: photos,
-			};
-			console.log(inputData);
-
-			const review = new Review({
-				user: req.user._id,
-				product: req.params.id,
-				...inputData,
-			});
-			await review.save();
-
-			return res.status(200).send("Review saved successfully");
+		req.files.forEach((file) => {
+			photos.push(file.filename);
 		});
 
-	form.parse(req);
-};
+		const review = new Review({
+			user: req.user._id,
+			product: req.params.id,
+			...req.body,
+			photos: photos,
+		});
 
-module.exports.getReviewImage = async (req, res) => {
-	const imagePath = path.resolve(
-		`${path.join(__dirname, "..storages/reviews")}/${req.params.name}`
-	);
-	const fileCheck = fs.existsSync(filePath);
-
-	if (fileCheck) {
-		return res.status(200).send(imagePath);
-	} else {
-		return res.status(404).send("No image found");
-	}
+		await review.save();
+		return res.status(200).send("Review created successfully!");
+	});
 };
