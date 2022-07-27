800df68bf6dc29aad35b4ccd3fb9962c091d076e
iammeskat <meskatcse@gmail.com>
2022-07-27 21:01:53 +0600

    small changes

--- controllers/adminControllers/dashboardController.js
+++ controllers/adminControllers/dashboardController.js
@@ -1,19 +1,41 @@
 const { Brand } = require('../../models/brand');
+const { Category } = require('../../models/category');
+const { Employee } = require('../../models/employee');
 const { Product } = require('../../models/product');
 const { Review } = require('../../models/review');
 const { User } = require('../../models/user');
+const { Order } = require('../../models/order');
 
 let dashboard = {};
 dashboard.shortSummary = async (req, res) => {
     
     try {
         let data = {};
-        data.user = await User.count();
-        data.brand = await Brand.count();
+        // data.totalUser = await User.count();
+        // data.totalBrand = await Brand.count();
+        // data.totalCategory = await Category.count();
+        // data.totalEmployee = await Employee.count();
+        // calculating total sales
+        let orders = await Order.find({ status: 'delivered' })
+            .populate('cartItem.product', 'price')
+            .select({
+                "cartItem": 1,
+                "discount": 1,
+                "status": 1
+            });
+        let totalSale = 0;
+        orders.forEach(order => {
+            let subTotal = 0;
+            order.cartItem.forEach(item => {
+                subTotal += item.product.price * item.count
+            })
+            subTotal = subTotal-order.discount
+        });
         
         res.json({
-            data,
-            message:"Product was added successfully!",
+            // data,
+            orders: orders,
+            message:"Short summary fetched",
             error: false
         })
     }catch(err){
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ global.__basedir = __dirname;
 const port = process.env.PORT || 3001;
 
 mongoose
-	.connect(process.env.MONGODB_SERVER_URL)
+	.connect(process.env.MONGODB_LOCAL_SERVER_URL)
 	.then(() => console.log("Connected to MongoDB server!!"))
 	.catch(() => console.error("ERROR connecting to MongoDB server!"));
 
